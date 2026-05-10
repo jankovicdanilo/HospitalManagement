66a04802c90714575cb580a5a97b5c41c443b215
danilo <jankovic.danilo23@gmail.com>
2026-05-10 22:22:08 +0200

    patient getall and get by id

---
+++ HospitalManagement/Repositories/Interfaces/IPatientRepository.cs
@@ -0,0 +1,10 @@
+﻿using HospitalManagement.Models.Domain;
+
+namespace HospitalManagement.Repositories.Interfaces
+{
+    public interface IPatientRepository
+    {
+        Task<List<Patient>> GetAllAsync();
+        Task<Patient?> GetByIdAsync(int id);
+    }
+}
---
+++ HospitalManagement/Services/Interfaces/IPatientService.cs
@@ -0,0 +1,11 @@
+﻿using HospitalManagement.Common;
+using HospitalManagement.Models.DTOs.Patient;
+
+namespace HospitalManagement.Services.Interfaces
+{
+    public interface IPatientService
+    {
+        Task<Result<List<PatientListDto>>> GetAllAsync();
+        Task<Result<PatientGetByIdDto?>> GetByIdAsync(int id);
+    }
+}
