935545850436b1aa8709eb56eceac8a60a2ff073
danilo <jankovic.danilo23@gmail.com>
2026-05-13 13:44:59 +0200

    Resolve merge conflicts

--- HospitalManagement/Repositories/Interfaces/IPatientRepository.cs
+++ HospitalManagement/Repositories/Interfaces/IPatientRepository.cs
@@ -5,7 +5,7 @@ namespace HospitalManagement.Repositories.Interfaces
     public interface IPatientRepository
     {
         Task<Patient?> CreateAsync(Patient patient);
-
+        
         Task<Patient> GetByEmail(string email);
     }
 }
--- HospitalManagement/Services/Implementations/PatientService.cs
+++ HospitalManagement/Services/Implementations/PatientService.cs
@@ -9,7 +9,7 @@ namespace HospitalManagement.Services.Implementations
     public class PatientService : IPatientService
     {
         private readonly IPatientRepository patientRepository;
-
+        
         public PatientService(IPatientRepository patientRepository)
         {
             this.patientRepository = patientRepository;
