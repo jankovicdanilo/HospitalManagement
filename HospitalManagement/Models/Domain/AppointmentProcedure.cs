e6ea6a15067b33a66c6f66eb11f6a6129a7f5291
Danilo Jankovic <jankovic.danilo23@gmail.com>
2026-06-03 08:28:26 +0200

    Add procedures table

---
+++ HospitalManagement/Models/Domain/AppointmentProcedure.cs
@@ -0,0 +1,11 @@
+﻿namespace HospitalManagement.Models.Domain
+{
+    public class AppointmentProcedure
+    {
+        public int AppointmentId { get; set; }
+        public Appointment Appointment { get; set; } = null!;
+
+        public int ProcedureId { get; set; }
+        public Procedure Procedure { get; set; } = null!;
+    }
+}
