f1cf922f4ca7dc6735cfcc4e4a2ed4af9ecc5da3
Danilo Jankovic <jankovic.danilo23@gmail.com>
2026-06-03 18:05:44 +0200

    Setup for unit testing

---
+++ HospitalManagement.Tests/Services/PatientServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using HospitalManagement.Repositories.Interfaces;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using HospitalManagement.Services.Implementations;
+
+namespace HospitalManagement.Tests.Services
+{
+    [TestFixture]
+    internal class PatientServiceTests
+    {
+        private Mock<IPatientRepository> patientRepositoryMock;
+        private Mock<IMapper> mapperMock;
+        private Mock<ILogger<PatientService>> loggerMock;
+        private PatientService patientService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            patientRepositoryMock = new Mock<IPatientRepository>();
+            mapperMock = new Mock<IMapper>();
+            loggerMock = new Mock<ILogger<PatientService>>();
+
+            patientService = new PatientService
+                (
+                    patientRepositoryMock.Object,
+                    mapperMock.Object,
+                    loggerMock.Object
+                );
+        }
+
+    }
+}
