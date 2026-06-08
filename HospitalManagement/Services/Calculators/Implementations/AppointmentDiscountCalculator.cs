df2e668aea8f9e579816045636b5b389bba90ab8
Danilo Jankovic <jankovic.danilo23@gmail.com>
2026-06-08 23:06:17 +0200

    Add discount to appointments

---
+++ HospitalManagement/Services/Calculators/Implementations/AppointmentDiscountCalculator.cs
@@ -0,0 +1,39 @@
+﻿using HospitalManagement.Models.Domain;
+using HospitalManagement.Services.Calculators.Interfaces;
+using HospitalManagement.Services.Calculators.Results;
+
+namespace HospitalManagement.Services.Calculators.Implementations
+{
+    public class AppointmentDiscountCalculator : IAppointmentDiscountCalculator
+    {
+        public DiscountResult Calculate(ICollection<AppointmentProcedure> procedures)
+        {
+            
+            var totalCost = procedures.Sum(ap => ap.Procedure.Price);
+            var procedureCount = procedures.Count;
+            decimal discount = 0;
+
+            if(procedureCount >= 3)
+            {
+                discount = totalCost * 0.03m;
+            }
+            if(procedureCount >= 5)
+            {
+                discount = totalCost * 0.04m;
+            }
+            if(procedureCount >= 7)
+            {
+                discount = totalCost * 0.05m;
+            }
+            if(discount > 100)
+            {
+                discount = 100;
+            }
+
+            totalCost = totalCost - discount;
+            var result = new DiscountResult(totalCost, discount);
+
+            return result;
+        }
+    }
+}
