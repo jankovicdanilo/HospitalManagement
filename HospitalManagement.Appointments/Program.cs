6455ea523771394f1da9fd2335408d8d817a0bb1
danilo jankovic <jankovic.danilo23@gmail.com>
2026-06-18 21:39:30 +0200

    Add appointment microservice

--- HospitalManagement.Appointments/Program.cs
+++ HospitalManagement.Appointments/Program.cs
@@ -1,175 +1,66 @@
-using FluentValidation;
-using HospitalManagement.Appointments.Clients.Implementations;
-using HospitalManagement.Appointments.Clients.Interfaces;
 using HospitalManagement.Appointments.Data;
-using HospitalManagement.Appointments.Models.DTOs.Appointment;
-using HospitalManagement.Appointments.Repositories.Implementations;
-using HospitalManagement.Appointments.Repositories.Interfaces;
-using HospitalManagement.Appointments.Services.Background;
-using HospitalManagement.Appointments.Services.Calculators.Implementations;
-using HospitalManagement.Appointments.Services.Calculators.Interfaces;
-using HospitalManagement.Appointments.Services.Implementations;
-using HospitalManagement.Appointments.Services.Interfaces;
-using HospitalManagement.Appointments.Services.Validations;
-using HospitalManagement.Appointments.Settings;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using NLog;
 using NLog.Web;
-using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromFile("nlog.config").GetCurrentClassLogger();
 
-builder.Services.AddDbContext<AppointmentDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AppointmentDb")));
-
-// Repositories
-builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
-builder.Services.AddScoped<IAppointmentProcedureRepository, AppointmentProcedureRepository>();
-builder.Services.AddScoped<ITreatmentRepository, TreatmentRepository>();
-
-// Services
-builder.Services.AddScoped<IAppointmentService, AppointmentService>();
-builder.Services.AddScoped<IAppointmentProcedureService, AppointmentProcedureService>();
-builder.Services.AddScoped<ITreatmentService, TreatmentService>();
-
-// Validations
-builder.Services.AddScoped<IAppointmentValidation, AppointmentValidation>();
-builder.Services.AddScoped<IAppointmentProcedureValidation, AppointmentProcedureValidation>();
-builder.Services.AddScoped<ITreatmentValidation, TreatmentValidation>();
-
-// Calculators
-builder.Services.AddScoped<IAppointmentDiscountCalculator, AppointmentDiscountCalculator>();
-
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddTransient<HospitalManagement.Shared.Http.AuthTokenHandler>();
-
-// HTTP client for cross-service calls to main API
-builder.Services.AddHttpClient<IMainApiClient, MainApiClient>(client =>
+try
 {
-    client.BaseAddress = new Uri(builder.Configuration["MainApi:BaseUrl"]!);
-})
-    .AddHttpMessageHandler<HospitalManagement.Shared.Http.AuthTokenHandler>();
+    var builder = WebApplication.CreateBuilder(args);
 
-// Background services
-builder.Services.AddHostedService<MissedAppointmentBackgroundService>();
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
 
-builder.Services.AddAutoMapper(typeof(Program));
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-builder.Logging.ClearProviders();
-builder.Host.UseNLog();
+    builder.Services.AddDbContext<AppointmentDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("AppointmentDb")));
 
-// Read JWT settings for token validation
-var jwtSettings = new JwtSettings
-{
-    Key = builder.Configuration["Jwt:Key"]!,
-    Issuer = builder.Configuration["Jwt:Issuer"]!,
-    Audience = builder.Configuration["Jwt:Audience"]!,
-    ExpiryMinutes = int.Parse(builder.Configuration["Jwt:ExpiryMinutes"]!)
-};
+    builder.Services.AddAutoMapper(typeof(Program));
 
-builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
-builder.Services.Configure<DiscountSettings>(builder.Configuration.GetSection("DiscountSettings"));
-builder.Services.Configure<AppointmentSettings>(builder.Configuration.GetSection("AppointmentSettings"));
+    // TODO: register IAppointmentRepository/AppointmentRepository,
+    // IAppointmentProcedureRepository/AppointmentProcedureRepository,
+    // IAppointmentService/AppointmentService,
+    // IAppointmentProcedureService/AppointmentProcedureService,
+    // IAppointmentValidation/AppointmentValidation,
+    // IAppointmentDiscountCalculator/AppointmentDiscountCalculator
+    // once those files are moved over
 
-// Configure JWT authentication
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.Events = new JwtBearerEvents
+    // TODO: JWT auth setup (copy from main API's Program.cs, since appointment
+    // endpoints will presumably still require authentication)
+
+    builder.Services.Configure<DiscountSettings>(builder.Configuration.GetSection("DiscountSettings"));
+    builder.Services.Configure<AppointmentSettings>(builder.Configuration.GetSection("AppointmentSettings"));
+
+    var app = builder.Build();
+
+    using (var scope = app.Services.CreateScope())
     {
-        OnChallenge = async context =>
-        {
-            context.HandleResponse();
-            context.Response.StatusCode = 401;
-            context.Response.ContentType = "application/json";
-            await context.Response.WriteAsJsonAsync(new
-            {
-                ErrorCode = "UNAUTHORIZED",
-                Message = "You must be logged in to access this resource"
-            });
-        }
-    };
+        var db = scope.ServiceProvider.GetRequiredService<AppointmentDbContext>();
+        db.Database.Migrate();
+    }
 
-    options.TokenValidationParameters = new TokenValidationParameters
+    if (app.Environment.IsDevelopment())
     {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = jwtSettings.Issuer,
-        ValidAudience = jwtSettings.Audience,
-        IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(jwtSettings.Key))
-    };
-});
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-builder.Services.AddAuthorization(options =>
-{
-    options.FallbackPolicy = new AuthorizationPolicyBuilder()
-        .RequireAuthenticatedUser().Build();
-});
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.MapControllers();
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters
-            .Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
-    });
-
-builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
-        Description = "Enter your JWT token here"
-    });
-
-    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
-    {
-        {
-            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
-            {
-                Reference = new Microsoft.OpenApi.Models.OpenApiReference
-                {
-                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
-});
-
-var app = builder.Build();
-
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppointmentDbContext>();
-    db.Database.Migrate();
+    app.Run();
 }
-
-if (app.Environment.IsDevelopment())
+catch (Exception ex)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    logger.Error(ex, "Stopped program because of exception");
+    throw;
 }
-
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
+finally
+{
+    LogManager.Shutdown();
+}
