b5baf7b88ae541a4cd055941b0371830e9ee7e8d
danilo jankovic <jankovic.danilo23@gmail.com>
2026-06-18 21:39:30 +0200

    Add appointment microservice

--- HospitalManagement.Appointments/Repositories/Implementations/TreatmentRepository.cs
+++ HospitalManagement.Appointments/Repositories/Implementations/TreatmentRepository.cs
@@ -1,35 +1,6 @@
-﻿using HospitalManagement.Appointments.Data;
-using HospitalManagement.Appointments.Models.Domain;
-using HospitalManagement.Appointments.Repositories.Interfaces;
-using Microsoft.EntityFrameworkCore;
-
-namespace HospitalManagement.Appointments.Repositories.Implementations
+﻿namespace HospitalManagement.Appointments.Repositories.Implementations
 {
-    public class TreatmentRepository : ITreatmentRepository
+    public class TreatmentRepository
     {
-        private readonly AppointmentDbContext dbContext;
-
-        public TreatmentRepository(AppointmentDbContext dbContext)
-        {
-            this.dbContext = dbContext;
-        }
-
-        public async Task<Treatment> CreateAsync(Treatment treatment)
-        {
-            treatment.CreatedAt = DateTime.UtcNow;
-            await dbContext.AddAsync(treatment);
-            await dbContext.SaveChangesAsync();
-            return treatment;
-        }
-
-        public async Task<Treatment?> GetByIdAsync(int id)
-        {
-            return await dbContext.Treatments.FirstOrDefaultAsync(x => x.Id == id);
-        }
-
-        public async Task<bool> TreatmentExists(int appointmentId)
-        {
-            return await dbContext.Treatments.AnyAsync(x => x.AppointmentId == appointmentId);
-        }
     }
 }
