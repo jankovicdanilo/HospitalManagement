e554f9140e1f63a4838f8dbcdfe27269fd0bb3f6
danilo jankovic <jankovic.danilo23@gmail.com>
2026-06-19 13:26:16 +0200

    Add AllowAnonymous to lookup endpoints, remove appointment entities migration

--- HospitalManagement/Migrations/20260619101145_RemoveAppointmentEntities.Designer.cs
+++ HospitalManagement/Migrations/20260619101145_RemoveAppointmentEntities.Designer.cs
@@ -4,6 +4,7 @@ using HospitalManagement.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HospitalManagement.Migrations
 {
     [DbContext(typeof(HospitalDbContext))]
-    partial class HospitalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260619101145_RemoveAppointmentEntities")]
+    partial class RemoveAppointmentEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
