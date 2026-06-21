4493438f07b49dfc554223a245b2c48d51d7fa34
danilo <jankovic.danilo23@gmail.com>
2026-06-21 16:51:03 +0200

    Add MassTransit with RabbitMQ, define event contracts in Shared, add RabbitMQ to docker-compose

---
+++ HospitalManagement.Shared/Events/PatientEvents.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Shared.Events
+{
+    public record PatientCreated
+    (
+        Guid CorrelationId,
+        int Id,
+        string FirstName,
+        string LastName,
+        string Email,
+        string? Phone,
+        DateOnly DateOfBirth
+    );
+
+    public record PatientUpdated
+    (
+        Guid CorrelationId,
+        int Id,
+        string FirstName,
+        string LastName,
+        string Email,
+        string? Phone
+    );
+
+    public record PatientDeleted
+    (
+        Guid CorrelationId,
+        int Id
+    );
+}
---
+++ HospitalManagement.Shared/Events/ProcedureEvents.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Shared.Events
+{
+    public record ProcedureCreated
+    (
+        Guid CorrelationId,
+        int Id,
+        string Name,
+        decimal Price
+    );
+
+    public record ProcedureUpdated
+    (
+        Guid CorrelationId,
+        int Id,
+        string Name,
+        decimal Price
+    );
+
+    public record ProcedureDeleted
+    (
+        Guid CorrelationId,
+        int Id
+    );
+}
