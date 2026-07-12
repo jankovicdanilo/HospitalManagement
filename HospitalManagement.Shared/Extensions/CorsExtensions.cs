61e1634a93bbe7a46ff1bbf25664d043a7c8cdc2
danilo <jankovic.danilo23@gmail.com>
2026-07-12 15:53:24 +0200

    Add cors in shared project for frontend

---
+++ HospitalManagement.Shared/Extensions/CorsExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement.Shared.Extensions
+{
+    public static class CorsExtensions
+    {
+        public static IServiceCollection AddFrontendCors(this IServiceCollection services)
+        {
+            return services.AddCors(options =>
+            {
+                options.AddPolicy("AllowFrontend", policy =>
+                {
+                    policy.WithOrigins("http://localhost:5173", "http://localhost:5174")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+        }
+    }
+}
