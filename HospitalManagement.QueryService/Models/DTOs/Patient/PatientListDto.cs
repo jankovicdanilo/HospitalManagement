3bb7e6e7fae4000a39288f5450be7e843b72086f
danilo jankovic <jankovic.danilo23@gmail.com>
2026-07-22 11:51:16 +0200

    Add phone and dateofbirth fields to list dto for patients

--- HospitalManagement.QueryService/Models/DTOs/Patient/PatientListDto.cs
+++ HospitalManagement.QueryService/Models/DTOs/Patient/PatientListDto.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; } = null!;
         public string LastName { get; set; } = null!;
         public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public DateOnly DateOfBirth { get; set; }
     }
 }
