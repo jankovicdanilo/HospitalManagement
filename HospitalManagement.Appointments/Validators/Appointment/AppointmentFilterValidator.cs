e316f81a2b236ae68d5f03583a907a7ff2eaa30c
danilo <jankovic.danilo23@gmail.com>
2026-08-11 14:25:08 +0200

    Add start end date logic for getall and appointments repository tests for getall

---
+++ HospitalManagement.Appointments/Validators/Appointment/AppointmentFilterValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using HospitalManagement.Appointments.Models.DTOs.Appointment;
+
+namespace HospitalManagement.Appointments.Validators.Appointment
+{
+    public class AppointmentFilterValidator : AbstractValidator<AppointmentFilterDto>
+    {
+        public AppointmentFilterValidator()
+        {
+            RuleFor(x => x)
+                .Must(x => (x.StartDate.HasValue && x.EndDate.HasValue) || (!x.StartDate.HasValue && !x.EndDate.HasValue))
+                .WithMessage("StartDate and EndDate must both be provided together, or both omitted");
+
+            RuleFor(x => x.EndDate)
+                .GreaterThanOrEqualTo(x => x.StartDate)
+                .When(x => x.StartDate.HasValue && x.EndDate.HasValue)
+                .WithMessage("EndDate must be on or after StartDate");
+
+            RuleFor(x => x.PageNumber).GreaterThan(0);
+            RuleFor(x => x.PageSize).InclusiveBetween(1, 100);
+        }
+    }
+}
